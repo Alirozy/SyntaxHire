e9cfb0f58aed52ca5b78c6768769901913b872c9
Ali Rozy <20243114@std.neu.edu.tr>
2026-07-04 21:23:43 +0500

    feat: initialize project structure and setup dockerized postgres environment

--- SyntaxHire_/SyntaxHire_/settings.py
+++ SyntaxHire_/SyntaxHire_/settings.py
@@ -9,21 +9,31 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
+import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Environ'ı başlat
+env = environ.Env(
+    # Varsayılan değerler tanımlayabilirsin
+    DEBUG=(bool, False)
+)
+
+# .env dosyasının yerini göster
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1sm!*8tb8q7+=mddt*+tni+-818=2l)6-lx#0jyvolau*1(y^2'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -74,8 +84,12 @@ WSGI_APPLICATION = 'SyntaxHire_.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
     }
 }
 
